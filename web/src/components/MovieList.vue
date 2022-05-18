55a0af71acbe22f659ba7a7b6b0ec0eb50d70830
Jakub Nowak <jakubnowak712@gmail.com>
2022-05-18 19:33:44 +0200

    indentation 4 spaces -> 2

--- web/src/components/MovieList.vue
+++ web/src/components/MovieList.vue
@@ -1,36 +1,36 @@
 <template>
 
 <table>
-    <tr v-for="(movie, index) in movies" v-bind:key="index">
-        <td>{{movie.year}}</td>
-        <td>{{movie.title}}</td>
-        <button @click="deleteMovie(movie)">Usuń</button>
-    </tr>
+  <tr v-for="(movie, index) in movies" v-bind:key="index">
+    <td>{{movie.year}}</td>
+    <td>{{movie.title}}</td>
+    <button @click="deleteMovie(movie)">Usuń</button>
+  </tr>
 </table>
 
 </template>
 
 <script>
 export default {
-    name: 'MovieList',
-    data() {
-        return {
-            movies: [
-                {title: "Drive", year: 2011},
-                {title: "2001: Odyseja kosmiczna", year: 1968},
-                {title: "Batman", year: 2022},
-                {title: "Batman", year: 1989},
-            ]
-        }
-    },
-    methods: {
-        deleteMovie(movie) {
-            if (!confirm(
-                `Czy na pewno chcesz usunąc film ${movie.title} (${movie.year})?`
-            )) return;
-            this.movies = this.movies.filter((m) => m != movie )
-        }
+  name: 'MovieList',
+  data() {
+    return {
+      movies: [
+        {title: "Drive", year: 2011},
+        {title: "2001: Odyseja kosmiczna", year: 1968},
+        {title: "Batman", year: 2022},
+        {title: "Batman", year: 1989},
+      ]
     }
+  },
+  methods: {
+    deleteMovie(movie) {
+      if (!confirm(
+        `Czy na pewno chcesz usunąc film ${movie.title} (${movie.year})?`
+      )) return;
+      this.movies = this.movies.filter((m) => m != movie )
+    }
+  }
 }
 </script>
 
--- web/src/components/MovieModal.vue
+++ web/src/components/MovieModal.vue
@@ -6,11 +6,11 @@
 <script>
 
 export default {
-    name: 'MovieModal',
-    data() {
-        return {
-        }
-    }
+	name: 'MovieModal',
+	data() {
+		return {
+		}
+	}
 }
 </script>
 
