4ed4ddf86c352d6adadae4b48cfd12dd3629327d
Jakub Nowak <jakubnowak712@gmail.com>
2022-05-18 20:46:52 +0200

    front: added modal input elements

--- web/src/components/MovieModal.vue
+++ web/src/components/MovieModal.vue
@@ -1,6 +1,12 @@
 <template>
 <div>
-  <p>{{mode}}</p>
+  <p>Tryb: {{mode}}</p>
+  Tytuł: <input type="text" v-bind="movie.title">
+  <br>
+  Rok: <input type="number" v-bind="movie.date">
+  <br>
+  <button v-show='mode != "preview"'>{{getButtonText()}}</button>
+
   <button @click="close">X</button>
 
 </div>
@@ -9,16 +15,26 @@
 
 <script>
 
+const buttonText = new Map([
+  ['add', 'Dodaj film'],
+  ['edit', 'Wprowadź zmiany'],
+])
+
 export default {
 	name: 'MovieModal',
 	data() {
 		return {
+      movie: {title: '', date: undefined}
 		}
 	},
   props: ["mode"],
   methods: {
     close() {
       this.$emit("closeModal")
+    },
+    getButtonText() {
+      console.log(this.mode)
+      return buttonText.get(this.mode)
     }
   }
 }
